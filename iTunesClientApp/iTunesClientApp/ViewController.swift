8a3d85b24489a09f497532e28b31acb6436363a8
onurduyar <onurdyr510@gmail.com>
2023-03-10 15:45:37 +0300

    project was initialized

---
+++ iTunesClientApp/iTunesClientApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesClientApp
+//
+//  Created by Onur Duyar on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
