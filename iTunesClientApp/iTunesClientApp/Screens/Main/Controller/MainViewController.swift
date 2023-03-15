5c70372ecd5c6b31e67d3bac6791bd413b8b8f3c
onurduyar <onurdyr510@gmail.com>
2023-03-15 18:15:40 +0300

    UICollectionView was added.

--- iTunesClientApp/iTunesClientApp/Screens/Main/Controller/MainViewController.swift
+++ iTunesClientApp/iTunesClientApp/Screens/Main/Controller/MainViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-
+    // MARK: - Properties
+    
+    private let mainView = MainView()
+    
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view = MainView()
-        view.backgroundColor = .red
+        view = mainView
+        mainView.setCollectionViewDelegate(delegate: self, andDataSource: self)
     }
+    
+    // MARK: - Methods
+    
+    
 
 
+}
+// MARK: - UICollectionViewDelegate
+
+extension MainViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("\(indexPath.row)")
+    }
+}
+// MARK: - UICollectionViewDataSource
+
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .gray
+        return cell
+    }
+    
+    
+    
 }
