a624a0b9dad614f315ed272ce614eac2cae702e3
onurduyar <onurdyr510@gmail.com>
2023-03-16 16:31:06 +0300

    DetailView was added.

---
+++ iTunesClientApp/iTunesClientApp/Screens/Detail/Controller/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  iTunesClientApp
+//
+//  Created by Onur Duyar on 16.03.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
---
+++ iTunesClientApp/iTunesClientApp/Screens/Detail/View/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  iTunesClientApp
+//
+//  Created by Onur Duyar on 16.03.2023.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
